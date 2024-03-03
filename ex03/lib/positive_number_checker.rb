32b7d3473bdaf036b0ab37fade254f506a5ae84a
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2024-03-03 13:49:21 -0300

    Add exercise 03 solution

--- ex03/lib/positive_number_checker.rb
+++ ex03/lib/positive_number_checker.rb
@@ -1,3 +1,18 @@
 def positive_number_checker(number)
-
+  return "Positivo" if number > 0
+  return "Negativo" if number < 0
+    
+  
+  "Neutro"
 end
+
+# Número positivo: número maior que 0 
+# Número negativo: número menor que 0
+# Número neutro: número é igual a 0
+
+# Se o número > 0 # Verdadeiro
+  # Imprimir "Positivo"
+# Se numero < 0 # Verdadeiro
+  # Imprimir "Negativo"
+# Senão
+  # Imprimir "Neutro"
